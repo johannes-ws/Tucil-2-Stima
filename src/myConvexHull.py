d43c63d2cd5ceb1528d087eee6b59399d4cc8ad9
Johannes <johannessukiatmodjo20@gmail.com>
2022-02-28 00:22:21 +0700

    revisi besar

--- src/myConvexHull.py
+++ src/myConvexHull.py
@@ -1,10 +1,78 @@
-from math import hypot
+import numpy as np
+from scipy import linalg
 
-def jarak2titik(x1, x2):
-    return hypot(x2[0]-x1[0], x2[1]-x1[1])
+def kiri(p1, p2, p3):
+    return p1[0]*p2[1] + p3[0]*p1[1] + p2[0]*p3[1] - p3[0]*p2[1] - p2[0]*p1[1] - p1[0]*p3[1] > 0
 
-def kiriatas(x1, x2, x3):
-    return x1[0]*x2[1] + x3[0]*x1[1] + x2[0]*x3[1] - x3[0]*x2[1] - x2[0]*x1[1] - x1[0]*x3[1] > 0
+def kanan(p1, p2, p3):
+    return p1[0]*p2[1] + p3[0]*p1[1] + p2[0]*p3[1] - p3[0]*p2[1] - p2[0]*p1[1] - p1[0]*p3[1] < 0
 
-def kananbawah(x1, x2, x3):
-    return x1[0]*x2[1] + x3[0]*x1[1] + x2[0]*x3[1] - x3[0]*x2[1] - x2[0]*x1[1] - x1[0]*x3[1] < 0
+def jaraktitikkegaris(p1, p2, p3):
+    return linalg.norm(np.cross(p2-p1, p1-p3)) / linalg.norm(p2-p1)
+
+def divideandconquer(semua_bagian, bagian, titik1, titik2, solusi):
+    global hull
+    hull = solusi
+    if (len(bagian) == 0):
+        hull.append((titik1, titik2))
+    elif (len(bagian) == 1):
+        hull.append((titik1, bagian[0]))
+        hull.append((bagian[0], titik2))
+    else:
+        global all_part
+        global total_part
+        global titiksampel
+
+        jarakmaxtitikkegaris = 0
+        titiksampel = bagian[0]
+        for i in range(len(bagian)):
+            jarak = jaraktitikkegaris(titik1, titik2, bagian[i])
+            if (jarak >= jarakmaxtitikkegaris):
+                jarakmaxtitikkegaris = jarak
+                titiksampel = bagian[i]
+
+        all_part = semua_bagian
+        total_part = len(all_part)
+
+        a = total_part
+        all_part.append([])
+        total_part += 1
+
+        left = 0
+        right = 0
+        for i in range(len(bagian)):
+            if (kiri(titik1, titiksampel, bagian[i])):
+                left += 1
+            elif (kanan(titik1, titiksampel, bagian[i])):
+                right += 1
+        if (left < right):
+            for i in range(len(bagian)):
+                if (kiri(titik1, titiksampel, bagian[i])):
+                    all_part[a].append(bagian[i])
+        elif (left > right):
+            for i in range(len(bagian)):
+                if (kanan(titik1, titiksampel, bagian[i])):
+                    all_part[a].append(bagian[i])
+
+        b = total_part
+        all_part.append([])
+        total_part += 1
+
+        left = 0
+        right = 0
+        for i in range(len(bagian)):
+            if (kiri(titiksampel, titik2, bagian[i])):
+                left += 1
+            elif (kanan(titiksampel, titik2, bagian[i])):
+                right += 1
+        if (left < right):
+            for i in range(len(bagian)):
+                if (kiri(titiksampel, titik2, bagian[i])):
+                    all_part[b].append(bagian[i])
+        elif (left > right):
+            for i in range(len(bagian)):
+                if (kanan(titiksampel, titik2, bagian[i])):
+                    all_part[b].append(bagian[i])
+
+        divideandconquer(all_part, all_part[a], titik1, titiksampel, hull)
+        divideandconquer(all_part, all_part[b], titiksampel, titik2, hull)
